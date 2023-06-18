3371bd8e5eae161ef5a07a3b85d17927ab2c0d05
wonhee418 <cheeky4@naver.com>
2023-06-18 20:23:25 +0900

    feat : Aside 컴포넌트 구현

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -16,7 +16,7 @@ const PostItem: FC<Post> = ({ profileImg, user, createdAt, userInfo, title, desc
       <div className="flex flex-3 flex-col  gap-4">
         <div className="flex flex-col  gap-4">
           <h1 className=" text-heading overflow-hidden text-ellipsis line-clamp-1">{title}</h1>
-          <div className=" text-body overflow-hidden text-ellipsis line-clamp-3">{desc}</div>
+          <div className=" text-body overflow-hidden text-ellipsis line-clamp-3 ">{desc}</div>
         </div>
         <div className="flex flex-col">
           <div className="flex items-center gap-3">
